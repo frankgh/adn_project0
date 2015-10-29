6cf66684e690935da7ead74b681b83a6ca8d7832
frank.guerrero <frank.guerrero@gmail.com>
2015-10-29 13:12:57 -0700

    Added improvements to showSnackbar

--- app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
+++ app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,27 +18,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showSnackbar(final View view) {
+        if (view instanceof Button) {
+            final Button button = (Button) view;
+            String message = String.format(getString(R.string.app_launch_message), button.getText());
 
-        final int viewId = view.getId(); // The ID of the button
-        int messageId = R.string.wut; // The ID of the message that we want to show
-
-        if (viewId == R.id.spotifyStreamerButton) {
-            messageId = R.string.main_activity_spotify_streamer_button;
-        } else if (viewId == R.id.scoresAppButton) {
-            messageId = R.string.main_activity_scores_app_button;
-        } else if (viewId == R.id.libraryAppButton) {
-            messageId = R.string.main_activity_library_app_button;
-        } else if (viewId == R.id.buildItBiggerButton) {
-            messageId = R.string.main_activity_build_it_bigger_button;
-        } else if (viewId == R.id.xyzReaderButton) {
-            messageId = R.string.main_activity_xyz_reader_button;
-        } else if (viewId == R.id.capstoneButton) {
-            messageId = R.string.main_activity_capstone_button;
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                    .setAction(getString(R.string.snackbar_text), null)
+                    .show();
         }
-
-        final String snackbarText = getResources().getString(R.string.snackbar_text);
-        String message = String.format(getString(R.string.app_launch_message), getString(messageId));
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).setAction(snackbarText, null).show();
     }
 
 }
