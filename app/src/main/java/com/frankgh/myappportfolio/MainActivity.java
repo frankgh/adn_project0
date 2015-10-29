1b6ec6eaebac3506719c8d3b4e90446f38999121
frank.guerrero <frank.guerrero@gmail.com>
2015-10-29 13:00:54 -0700

    Applied code review revisions

--- app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
+++ app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,55 +14,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        bindClickListeners();
     }
 
-    private void bindClickListeners() {
+    public void showSnackbar(final View view) {
+
+        final int viewId = view.getId(); // The ID of the button
+        int messageId = R.string.wut; // The ID of the message that we want to show
+
+        if (viewId == R.id.spotifyStreamerButton) {
+            messageId = R.string.main_activity_spotify_streamer_button;
+        } else if (viewId == R.id.scoresAppButton) {
+            messageId = R.string.main_activity_scores_app_button;
+        } else if (viewId == R.id.libraryAppButton) {
+            messageId = R.string.main_activity_library_app_button;
+        } else if (viewId == R.id.buildItBiggerButton) {
+            messageId = R.string.main_activity_build_it_bigger_button;
+        } else if (viewId == R.id.xyzReaderButton) {
+            messageId = R.string.main_activity_xyz_reader_button;
+        } else if (viewId == R.id.capstoneButton) {
+            messageId = R.string.main_activity_capstone_button;
+        }
 
         final String snackbarText = getResources().getString(R.string.snackbar_text);
-        final Button spotifyStreamerButton = (Button) findViewById(R.id.spotifyStreamerButton);
-        final Button scoresAppButton = (Button) findViewById(R.id.scoresAppButton);
-        final Button libraryAppButton = (Button) findViewById(R.id.libraryAppButton);
-        final Button buildItBiggerButton = (Button) findViewById(R.id.buildItBiggerButton);
-        final Button xyzReaderButton = (Button) findViewById(R.id.xyzReaderButton);
-        final Button capstoneButton = (Button) findViewById(R.id.capstoneButton);
-
-        View.OnClickListener actionButtonListener = new View.OnClickListener() {
-            public void onClick(View view) {
-                int messageId = 0;
-
-                if (view == spotifyStreamerButton) {
-                    messageId = R.string.spotify_streamer_launch_message;
-                } else if (view == scoresAppButton) {
-                    messageId = R.string.scores_app_launch_message;
-                } else if (view == libraryAppButton) {
-                    messageId = R.string.library_app_launch_message;
-                } else if (view == buildItBiggerButton) {
-                    messageId = R.string.build_it_bigger_launch_message;
-                } else if (view == xyzReaderButton) {
-                    messageId = R.string.xyz_reader_launch_message;
-                } else if (view == capstoneButton) {
-                    messageId = R.string.capstone_launch_message;
-                } else {
-                    messageId = R.string.wut;
-                }
-
-                showSnackbar(view, messageId, snackbarText);
-            }
-        };
-
-        spotifyStreamerButton.setOnClickListener(actionButtonListener);
-        scoresAppButton.setOnClickListener(actionButtonListener);
-        libraryAppButton.setOnClickListener(actionButtonListener);
-        buildItBiggerButton.setOnClickListener(actionButtonListener);
-        xyzReaderButton.setOnClickListener(actionButtonListener);
-        capstoneButton.setOnClickListener(actionButtonListener);
-
-    }
-
-    private void showSnackbar(final View view, final int messageId, final String snackbarText) {
-        Snackbar.make(view, getResources().getString(messageId), Snackbar.LENGTH_LONG)
-                .setAction(snackbarText, null).show();
+        String message = String.format(getString(R.string.app_launch_message), getString(messageId));
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).setAction(snackbarText, null).show();
     }
 
 }
