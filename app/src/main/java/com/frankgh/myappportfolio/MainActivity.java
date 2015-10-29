b557889967aa89b327e87c74e1194796fef27102
frank.guerrero <frank.guerrero@gmail.com>
2015-10-29 10:22:08 -0700

    Minor cleaning up

--- app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
+++ app/src/main/java/com/frankgh/myappportfolio/MainActivity.java
@@ -21,48 +21,41 @@ public class MainActivity extends AppCompatActivity {
     private void bindClickListeners() {
 
         final String snackbarText = getResources().getString(R.string.snackbar_text);
-
         final Button spotifyStreamerButton = (Button) findViewById(R.id.spotifyStreamerButton);
-        spotifyStreamerButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                showSnackbar(view, R.string.spotify_streamer_launch_message, snackbarText);
-            }
-        });
-
         final Button scoresAppButton = (Button) findViewById(R.id.scoresAppButton);
-        scoresAppButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                showSnackbar(view, R.string.scores_app_launch_message, snackbarText);
-            }
-        });
-
         final Button libraryAppButton = (Button) findViewById(R.id.libraryAppButton);
-        libraryAppButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                showSnackbar(view, R.string.library_app_launch_message, snackbarText);
-            }
-        });
-
         final Button buildItBiggerButton = (Button) findViewById(R.id.buildItBiggerButton);
-        buildItBiggerButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                showSnackbar(view, R.string.build_it_bigger_launch_message, snackbarText);
-            }
-        });
-
         final Button xyzReaderButton = (Button) findViewById(R.id.xyzReaderButton);
-        xyzReaderButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                showSnackbar(view, R.string.xyz_reader_launch_message, snackbarText);
-            }
-        });
-
         final Button capstoneButton = (Button) findViewById(R.id.capstoneButton);
-        capstoneButton.setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener actionButtonListener = new View.OnClickListener() {
             public void onClick(View view) {
-                showSnackbar(view, R.string.capstone_launch_message, snackbarText);
+                int messageId = 0;
+
+                if (view == spotifyStreamerButton) {
+                    messageId = R.string.spotify_streamer_launch_message;
+                } else if (view == scoresAppButton) {
+                    messageId = R.string.scores_app_launch_message;
+                } else if (view == libraryAppButton) {
+                    messageId = R.string.library_app_launch_message;
+                } else if (view == buildItBiggerButton) {
+                    messageId = R.string.build_it_bigger_launch_message;
+                } else if (view == xyzReaderButton) {
+                    messageId = R.string.xyz_reader_launch_message;
+                } else {
+                    messageId = R.string.capstone_launch_message;
+                }
+
+                showSnackbar(view, messageId, snackbarText);
             }
-        });
+        };
+
+        spotifyStreamerButton.setOnClickListener(actionButtonListener);
+        scoresAppButton.setOnClickListener(actionButtonListener);
+        libraryAppButton.setOnClickListener(actionButtonListener);
+        buildItBiggerButton.setOnClickListener(actionButtonListener);
+        xyzReaderButton.setOnClickListener(actionButtonListener);
+        capstoneButton.setOnClickListener(actionButtonListener);
 
     }
 
